e87b58de915f85098837de9c0cf5bb840e4cccbf
aqifsariman <aqifsariman@gmail.com>
2023-02-25 00:07:35 +0800

    Completed Top Headlines and designing of page.

--- src/main/java/ibf2022/ssf/newsapi/controllers/TopNewsController.java
+++ src/main/java/ibf2022/ssf/newsapi/controllers/TopNewsController.java
@@ -8,23 +8,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import ibf2022.ssf.newsapi.models.News;
 import ibf2022.ssf.newsapi.services.NewsServices;
 
 @Controller
-@RequestMapping(path = "/everything")
-public class NewsController {
-
+@RequestMapping(path = "/top-headlines")
+public class TopNewsController {
     @Autowired
     private NewsServices newsSvc;
 
     @GetMapping
-    public String getEverything(Model model, @RequestParam String q) {
-        Optional<List<News>> opt = newsSvc.getEverything(q);
+    public String getEverything(Model model) {
+        Optional<List<News>> opt = newsSvc.getTopHeadlines();
         List<News> newsList = opt.get();
         model.addAttribute("newsList", newsList);
-        return "everything";
+        return "top-headlines";
     }
 }
